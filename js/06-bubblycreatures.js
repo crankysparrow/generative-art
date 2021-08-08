699fef2e3d99dfc8806c20dd0c2f1fb0c9207dbb
Michelle <michelle.en4@gmail.com>
2021-08-08 15:44:22 -0400

    idk bubbles

--- js/06-bubblycreatures.js
+++ js/06-bubblycreatures.js
@@ -3,8 +3,11 @@
 let pos = [];
 const cols = ['#ff0000', '#00ff00', '#0000ff'];
 
+
 function setup () {
   createCanvas(window.innerWidth, window.innerHeight);
+  currentX = width/2
+  currentY = height/2
 }
 
 function windowResized () {
@@ -18,30 +21,35 @@ function draw(){
   
   fill(255); 
   noStroke();
+
   
-  for (let i = 0; i <=20; i++) {
+  for (let i = 0; i <=15; i++) {
     fill(cols[i%3]);
     if (!pos[i]) {
-      let lastX = random(10);
-      let lastY = random(10);
-      pos[i] = [0,0, lastX, lastY];
+      let lastX = random(width);
+      let lastY = random(height);
+      pos[i] = [lastX, lastY, lastX, lastY ];
     } else { 
-      pos[i] = circleCreature(pos[i]);
+      pos[i] = circleCreature(pos[i], i);
     }
   }
 
 } 
 
-function circleCreature(position) {
-  // let newY = mouseY + random(position[3] * -50, 0);
+function circleCreature(position, i) {
   let newY = noise(mouseY + position[3]) * height;
   let newX = noise(mouseX + position[2]) * width;
-  // let newX = mouseX + random(position[2] * 50, 0);
-    
+
+  // let noiseY = (noise(nY+i) * variance) - (variance/2)
+  // let noiseX = (noise(nX+i) * variance) - (variance/2)
+  // let newX = mouseX + (random(1, -1) * noiseX )
+  // let newY = mouseY + (random(1,-1) * noiseY)
+
   let x = lerp(position[0], newX, 0.07);
   let y = lerp(position[1], newY, 0.07);
   blendMode(SCREEN);
-  circle(x,y,130);
+  circle(x, y, 90);
   // return [x,y, position[2] + random(-0.5, 0.5), position[3] + random(-0.5, 0.5)];
   return [x,y,position[2], position[3]];
+  // return [x, y]
 }
