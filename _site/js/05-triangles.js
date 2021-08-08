f10985af2672ff3df0e4a99714d87a59ba329809
Michelle <michelle.en4@gmail.com>
2021-08-08 09:38:56 -0400

    edited triangles

--- _site/js/05-triangles.js
+++ _site/js/05-triangles.js
@@ -1,78 +1,88 @@
 // feb 2021
 
-const MAX_HEIGHT = window.innerHeight / 2;
-const MAX_WIDTH = window.innerWidth / 3;
 const SIZE = 110;
-const FACTOR = 20;
+const FACTOR = 50;
 
-// Create a new canvas to the browser size
+let n = 0.1
+let n2 = 1.2
+let n3 = 2.3
+let n4 = 3.4
+let n5 = 4.1
+let noiseStep = 0.01
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  noLoop();
 }
 
-// On window resize, update the canvas size
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+function mouseClicked() {
+  redraw()
+}
 
 function draw() {
-  
+
+  blendMode(BLEND)
   noStroke();
   let colors = ['#C5FFFD', '#0DA8AB', '#7F2178', '#B143A8'];
+  background(colors[0])
+
+  n += noiseStep
+  n2 += noiseStep
+  n3 += noiseStep
+  n4 += noiseStep
+  n5 += noiseStep
   
-  colorMode(HSB, 100);
-  background(random(60, 70), 10, 100);
-  
-  colorMode(RGB, 255);
   let triangles = [];
+  blendMode(MULTIPLY)
+  fill(colors[3])
   
-
-  for (let y = SIZE; y <= height - SIZE*2; y+= SIZE) {
+  let i = 0
+  for (let y = SIZE/2; y <= height - SIZE*2; y+= SIZE) {
     for (let x = SIZE; x <= width - SIZE; x+= SIZE) { 
-      let f = random() * FACTOR - (FACTOR / 2);
-      let g = random() * FACTOR - (FACTOR / 2);
-      let h = random() * FACTOR - (FACTOR / 2);
-      let j = random() * FACTOR - (FACTOR / 2);
-      let k = random() * FACTOR - (FACTOR / 2);
 
-      triangles.push([
-        x-SIZE/2+f, y+SIZE/2+g, x+SIZE/2+h, y+SIZE/2+j, x+k, y+SIZE*1.5+f
-      ]);
+      i++
+      let noiseAdd = 0
+
+      let f = (noise( noiseAdd + n )  * FACTOR) - (FACTOR / 2);
+      let g = (noise( noiseAdd + n2 ) * FACTOR) - (FACTOR / 2);
+      let h = (noise( noiseAdd + n3 ) * FACTOR) - (FACTOR / 2);
+      let j = (noise( noiseAdd + n2 ) * FACTOR) - (FACTOR / 2);
+      let k = (noise( noiseAdd + n4 ) * FACTOR) - (FACTOR / 2);
+      let l = (noise( noiseAdd + n5 ) * FACTOR) - (FACTOR / 2);
+
+        triangle(x-SIZE/2+f, y+SIZE/2+g, x+SIZE/2+h, y+SIZE/2+j, x+k, y+SIZE*1.5+l)
+
     }
+  }
     
+  fill(colors[1])
+  i = 0
+  for (let y = SIZE; y <= height-SIZE*2; y+= SIZE) {
     for (let x = SIZE*1.5; x<= width - SIZE; x+= SIZE) {
-      let f = random() * FACTOR - (FACTOR / 2);
-      let g = random() * FACTOR - (FACTOR / 2);
-      let h = random() * FACTOR - (FACTOR / 2);
-      let j = random() * FACTOR - (FACTOR / 2);
-      let k = random() * FACTOR - (FACTOR / 2);
-      triangles.push([x+f, y+g, (x-SIZE/2)+h, y+SIZE+k, x+SIZE/2+j, y+SIZE+f]);
+
+      i++
+      let noiseAdd = 0
+
+      let f = (noise(noiseAdd+n) * FACTOR) - (FACTOR / 2);
+      let g = (noise(noiseAdd+n2) * FACTOR) - (FACTOR / 2);
+      let h = (noise(noiseAdd+n3) * FACTOR) - (FACTOR / 2);
+      let j = (noise(noiseAdd+n2) * FACTOR) - (FACTOR / 2);
+      let k = (noise(noiseAdd+n4) * FACTOR) - (FACTOR / 2);
+      let l = (noise(noiseAdd+n5) * FACTOR) - (FACTOR/2);
+      triangle(x+f, y+g, (x-SIZE/2)+h, y+SIZE+k, x+SIZE/2+j, y+SIZE+l)
     }
+
+  }
     
-    triangles = shuffle(triangles).reverse();
-    for (let i = 0; i < triangles.length; i++) {
-      if (i%3 == 0) {
-        blendMode(OVERLAY);
-      } else {
-        blendMode(BLEND);
-      }     
-      let c = colors[ Math.floor(random(0, colors.length)) ];
-      fill(c);         
-      triangle(...triangles[i]);
-    }
+    // triangles = shuffle(triangles);
+    // // for (let i = 0; i < triangles.length; i++) {
+    // //   fill(colors[2])
+    // //   triangle(...triangles[i]);
+    // }
 
   }
-}
 
-function shuffle(a) {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = a[i];
-      a[i] = a[j];
-      a[j] = x;
-  }
-  return a;
-}
+
