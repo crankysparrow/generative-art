5041e9a75733bf65231a6cb5aa0e47f1bfd40925
Michelle <michelle.en4@gmail.com>
2021-09-21 11:43:26 -0400

    sandbox

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,63 +1,22 @@
-const cols = ['#f5dc23', '#ed225d', '#1c1c1c']
-let tile
-
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight)
-	imageMode(CENTER)
-	angleMode(DEGREES)
+	colorMode(HSB)
+
 	noLoop()
 }
 
 function draw() {
-	let s = 50
-	let bigS = s * 3
+	let h = random(255)
+	let s = random(80, 100)
+	let b = random(50, 100)
+	let bg = color(h, s, b)
 
-	let bigTile = createGraphics(bigS, bigS)
+	background(bg)
 
-	let tile = createGraphics(s, s)
-	tile.fill(0)
-	tile.rect(0, 0, s, s)
-
-	bigTile.fill('#f0f')
-	bigTile.rect(0, 0, bigS, bigS)
-	bigTile.imageMode(CENTER)
-	bigTile.angleMode(DEGREES)
-
-	// inner tiles
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 3; j++) {
-			makeTile(i * s, j * s, s, bigTile)
+	for (let x = 0; x < width; x++) {
+		for (let y = 0; y < height; y++) {
+			stroke(h, s, b - 20, noise(x * y))
+			point(x, y)
 		}
 	}
-
-	// outer tiles
-	for (let xx = 0; xx < 2; xx++) {
-		for (let yy = 0; yy < 2; yy++) {
-			push()
-			translate(xx * bigS + bigS / 2, yy * bigS + bigS / 2)
-			image(bigTile, 0, 0)
-			pop()
-		}
-	}
-}
-
-function makeTile(x, y, s, bigTile) {
-	bigTile.push()
-	bigTile.translate(x + s / 2, y + s / 2)
-	bigTile.rotate(random([0, 90, 180, 270]))
-
-	tile = createGraphics(s, s)
-
-	tile.background(0)
-	tile.noStroke()
-	tile.fill(cols[1])
-	tile.triangle(0, 0, s, 0, 0, s)
-
-	bigTile.image(tile, 0, 0)
-
-	bigTile.pop()
-}
-
-function mousePressed() {
-	redraw()
 }
