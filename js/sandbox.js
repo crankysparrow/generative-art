8fe452f8aacf85f51bbfd27a3614213dfb85420e
Michelle <michelle.en4@gmail.com>
2021-10-19 14:03:27 -0400

    lalalaa

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,61 +1,76 @@
-let topShape
-let bottomShape
-let inc = 50
-let count = 0
-let points = []
-let n = 16
-let xMod = 1200
-let yMod = 850
+let url = 'https://coolors.co/e3d3e4-6320ee-2ec4b6-e71d36-ff9f1c-5c0029'
+let palette = paletteFromUrl(url)
 
+let p1, p2, pS, pE, m
+
+function easeOutSine(x) {
+	return Math.sin((x * Math.PI) / 2)
+}
+
+function paletteFromUrl(url) {
+	return url
+		.split('coolors.co/')
+		.slice(1)[0]
+		.split('-')
+		.map((c) => '#' + c)
+}
 
 function setup() {
-	createCanvas(800, 800)
+	createCanvas(window.innerWidth, window.innerHeight)
 
-	colorMode(HSB, 100)
+	m = min(width, height)
 
-	topShape = random(height * 0.05, height * 0.25)
-	bottomShape = random(height * 0.65, height * 0.95)
+	pS = createVector(m * 0.1, m * 0.9)
+	p1 = createVector(m * 0.8, m * -0.5)
+	p2 = createVector(m * 0.8, m * 1.4)
+	pE = createVector(m * 0.7, m * 0.1)
 
-	inc = (width - 40) / n
-
-	let i = 0
-	while (i < n) {
-		let x = i * inc
-		let y = i % 2 == 1 ? topShape : bottomShape
-		points.push(createVector(x, y))
-		i++
-	}
-
+	makePane()
 }
 
 function draw() {
-	translate(40, 0)
 	background(0)
-
-	// fill(255, 100)
+	stroke(255, 100)
 	noFill()
-	stroke(255, 10)
+
+	translate((width-m)/2, (height-m)/2)
+
+	// strokeWeight(5)
+	// point( p1.x, p1.y)
+	// point( p2.x, p2.y )
+
 	strokeWeight(2)
-	strokeJoin(ROUND)
+	for (let i = 0; i < 105; i++) {
+		let adjust = i * 2
 
-	count++
-	if (mouseIsPressed) {
-		count++
+		bezier(
+			pS.x + i, pS.y + i,
+			p1.x - adjust, p1.y - adjust,
+			p2.x - adjust, p2.y + adjust,
+			pE.x - i, pE.y - i
+		)
 	}
+}
 
-	let pointsR = []
-	for (let i = 0; i < points.length; i++) {
-		let newV = points[i].copy()
-		newV.x += noise(newV.x * 0.5, count * 0.003) * xMod - xMod / 2
-		newV.y += noise(i * 0.05, count * 0.002) * yMod - yMod / 2
-		pointsR.push(newV)
-	}
+function makePane() {
+	pane = new Tweakpane.Pane()
 
-	// fill(255, 100)
-	let i = 1
-	while (i < pointsR.length - 2) {
-		fill( i * 5, 100, 100, 20)
-		triangle(pointsR[i].x, pointsR[i].y, pointsR[i+1].x, pointsR[i+1].y, pointsR[i+2].x, pointsR[i+2].y)
-		i += 1
-	}
+	let p1Folder = pane.addFolder({
+		title: 'p1'
+	})
+	let p2Folder = pane.addFolder({
+		title: 'p2'
+	})
+	let pSFolder = pane.addFolder({ title: 'pS' })
+	let pEFolder = pane.addFolder({ title: 'pE' })
+
+	p1Folder.addInput(p1, 'x', { min: m * -0.5, max: m * 1.5, step: 1 })
+	p1Folder.addInput(p1, 'y', { min: m * -0.5, max: m * 1.5, step: 1 })
+	p2Folder.addInput(p2, 'x', { min: m * -0.5, max: m * 1.5, step: 1 })
+	p2Folder.addInput(p2, 'y', { min: m * -0.5, max: m * 1.5, step: 1 })
+	pSFolder.addInput(pS, 'x', { min: m * -0.5, max: m * 1.5, step: 1 })
+	pSFolder.addInput(pS, 'y', { min: m * -0.5, max: m * 1.5, step: 1 })
+	pEFolder.addInput(pE, 'x', { min: m * -0.5, max: m * 1.5, step: 1 })
+	pEFolder.addInput(pE, 'y', { min: m * -0.5, max: m * 1.5, step: 1 })
+
 }
