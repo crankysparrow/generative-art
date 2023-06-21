33c168a57f8f8b68b992ee08b790fc88a9dfafb3
Michelle <michelle.en4@gmail.com>
2023-06-20 22:02:08 -0400

    happyaccidents js

--- js/28-happy-accidents.js
+++ js/28-happy-accidents.js
@@ -1,11 +1,18 @@
 let seed = 1
 let m
+let circle1, circle2
+let p
 
 function setup() {
 	m = min(window.innerWidth, window.innerHeight)
 	createCanvas(window.innerWidth, window.innerHeight)
 
-	noLoop()
+	circle1 = new LinesCircle(m * 0.7, 0)
+	circle2 = new LinesCircle(m * 0.5, 0.7)
+
+	p = createP('')
+
+	// noLoop()
 }
 
 function draw() {
@@ -14,8 +21,18 @@ function draw() {
 	strokeWeight(2)
 	stroke(255, 50)
 
-	let circle1 = new LinesCircle(m * 0.7, 0).drawShape()
-	let circle2 = new LinesCircle(m * 0.5, 0.7).drawShape()
+	let w = mouseX / width
+	let h = mouseY / width
+
+	circle1.noiseOffset = w
+	circle2.noiseOffset = h
+	circle1.rotate = (w * PI) / 2
+
+	circle1.drawShape()
+	circle2.drawShape()
+
+	// let circle1 = new LinesCircle(m * 0.7, 0).drawShape()
+	// let circle2 = new LinesCircle(m * 0.5, 0.7).drawShape()
 }
 
 class LinesCircle {
@@ -24,34 +41,39 @@ class LinesCircle {
 		this.mag = this.magScale
 		this.noiseOffset = noiseOffset
 		this.a = 0
+		this.rotate = 0
 
-		this.inc = TWO_PI / 600
+		this.inc = TWO_PI / 299
 		this.drawing = true
 	}
 
-	drawLine() {
+	drawLine(a) {
 		// this.mag = this.mag + map(noise(this.noiseFactor), 0, 1, -1, 1)
-		let noiseFactor = abs(sin(this.a * 2)) + this.noiseOffset
+		let noiseFactor = abs(sin(a * 2)) + this.noiseOffset
 		// without different offsets, multiple patterns will look the same,
 		// they'll just be scaled larger or smaller depending on magnitude
 
 		let mValue = noise(noiseFactor)
 		this.mag = mValue * this.magScale
 
-		let v = p5.Vector.fromAngle(this.a, this.mag)
+		let v = p5.Vector.fromAngle(a, this.mag)
 		line(0, 0, v.x, v.y)
 
-		this.a += this.inc
+		// this.a += this.inc
 	}
 
 	drawShape() {
-		while (this.a < TWO_PI) {
-			this.drawLine()
-		}
+		strokeWeight(2)
+		stroke(255, 50)
 
-		if (this.a >= TWO_PI) {
-			console.log('this.a: ', this.a)
+		push()
+		rotate(this.rotate)
+		let a = 0
+		while (a < TWO_PI) {
+			this.drawLine(a)
+			a += this.inc
 		}
+		pop()
 	}
 }
 
